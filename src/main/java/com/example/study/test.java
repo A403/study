c03a1d3e86de5f5eaa4d2d37f233292b89721466
A403 <a403@njcit.me>
2022-05-18 14:00:10 +0800

    开始新的项目

--- src/main/java/com/example/study/test.java
+++ src/main/java/com/example/study/test.java
@@ -1,18 +1,15 @@
 package com.example.study;
-import com.example.study.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
-public class HelloController {
-    @Autowired
-    private Person person;
+public class test {
     @ResponseBody
-    @RequestMapping("/hello")
-    public Person hello() {
-
-        return person;
+    @RequestMapping("/test")
+    public String hello() {
+        String s = "s";
+        s = s + "123";
+        return s;
     }
 }
-
